7a64425609d58905e34d3ad6509b207245b3a055
EllaCodes4 <105608215+EllaCodes4@users.noreply.github.com>
2023-01-30 22:33:54 -0600

    Hour and minute hands now functional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 function setDate() {
   const secondsHand = document.querySelector(".second-hand");
+  const hourHand = document.querySelector(".hour-hand");
+  const minutesHand = document.querySelector(".min-hand");
 
   const now = new Date();
+
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds);
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
